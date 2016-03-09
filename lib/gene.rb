42894d407935b198de230901c896df50e7311a98
Stefanie Mühlhausen <s.muehlhausen@bath.ac.uk>
2016-03-09 15:39:10 +0000

    Initial commit gene.rb

---
+++ lib/gene.rb
@@ -0,0 +1,21 @@
+class Gene
+
+    def initialize(file)
+        @path = file
+        @translation = ""
+        @description = []
+        @exons = []
+        @introns = []
+
+        parse_file
+
+    end
+
+    def parse_file
+
+    end
+
+    def tweak_exons
+
+    end
+end
