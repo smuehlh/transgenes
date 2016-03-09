d845aa904b942422825914db1558d2141d6275c2
smuehlh <s.muehlhausen@bath.ac.uk>
2016-03-09 11:19:15 +0000

    Bugfix counting conversion

--- lib/generic_helper.rb
+++ lib/generic_helper.rb
@@ -3,10 +3,10 @@ module Counting
     extend self
 
     def human_to_ruby(int) 
-        int.to_i + 1
+        int.to_i - 1
     end
 
     def ruby_to_human(int)
-        int - 1
+        int + 1
     end
 end
