51154e13a7a3e6eb0c48de7a749aef62e944e98b
Stefanie Mühlhausen <s.muehlhausen@bath.ac.uk>
2016-03-09 15:14:48 +0000

    Revert "Fasta file parsing. Outline of ese-tweaking implemented"
    This reverts commit 724552c92191573d59e14da9245070e0083a8a7a.

--- lib/commandline_options.rb
+++ lib/commandline_options.rb
@@ -13,26 +13,23 @@ class CommandlineOptions
         @output = nil
 
         # call parser for every class instance
-        ensure_help_is_printed_if_no_options_given
         parse_options
     end
 
     def parse_options
         opt_parser = OptionParser.new do |opts|
-            opts.banner = "Alter synonymous sites to enhance transgenes."
+            opts.banner = "SequenceOptimiser enhances transgenes by removing ESEs"
             opts.separator "Contact: Laurence Hurst (l.d.hurst@bath.ac.uk)"
             opts.separator ""
             opts.separator "Usage: ruby sequence_optimiser.rb -i input -o output [options]"
 
-            opts.on("-i", "--input FILE",
-                "Path to input file in one of the following formats:",
-                "GeneBank record OR", 
-                "FASTA with exons in upper case and introns in lower case.") do |path|
+            opts.on("-i", "--input GFF",
+                "Path to input file, in GFF format.") do |path|
                 FileHelper.file_exist_or_die(path)
                 @input = path
             end
-            opts.on("-o", "--output FILE",
-                "Path to output file, in FASTA format.") do |path|
+            opts.on("-o", "--output GFF",
+                "Path to output file, in GFF format.") do |path|
                 @output = path
             end
 
@@ -59,13 +56,9 @@ class CommandlineOptions
             abort exc.to_s.capitalize
     end
 
-    def ensure_help_is_printed_if_no_options_given
-        @args.push "-h" if @args.empty?
-    end
-
     def ensure_mandatory_arguments_are_set
         @@mandatory_arguments.each do |arg|
-            opt_str = arg.sub("@", "--")
+            opt_str = arg.gsub("@", "--")
             abort "Missing mandatory option: '#{opt_str}'." if 
                 ! instance_variable_get(arg) 
         end
