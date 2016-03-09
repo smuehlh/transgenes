bb0685088dbfd7fb22d67e1dcf2551e96baa4188
smuehlh <s.muehlhausen@bath.ac.uk>
2016-03-09 11:15:27 +0000

    Adjusted white space

--- lib/file_helper.rb
+++ lib/file_helper.rb
@@ -5,9 +5,11 @@ module FileHelper
     def file_exist?(path)
         FileTest.file?(path)
     end
+
     def file_exist_or_die(path)
         abort "Failed to open: #{path}" if ! file_exist?(path)
     end
+    
     def split_filename_and_extension(path)
         basename = File.basename(path)
         extension = File.extname(basename)
