6afef3b67bdecb995b89f175d179326b86ac13b8
smuehlh <s.muehlhausen@bath.ac.uk>
2016-03-09 09:04:07 +0000

    Included fileextension handling.

--- lib/file_helper.rb
+++ lib/file_helper.rb
@@ -8,5 +8,9 @@ module FileHelper
     def file_exist_or_die(path)
         abort "Failed to open: #{path}" if ! file_exist?(path)
     end
-
+    def split_filename_and_extension(path)
+        basename = File.basename(path)
+        extension = File.extname(basename)
+        [ File.basename(basename, extension), extension ]
+    end
 end
