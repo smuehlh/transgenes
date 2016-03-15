94a0085ff1883c582aeac2b3cf2469fdddc15dd1
Stefanie Mühlhausen <s.muehlhausen@bath.ac.uk>
2016-03-15 13:32:39 +0000

    Bugfix code refactoring

--- lib/to_gene.rb
+++ lib/to_gene.rb
@@ -4,11 +4,6 @@ class ToGene
         # implement in sub-class
     end
 
-    def translate_exons(exons)
-        joined_exons = exons.join("")
-        AminoAcid.translate(joined_exons)
-    end
-
     def convert_exons_to_uppercase_and_introns_to_lowercase(exons, introns)
         exons.map!{ |e| e.upcase }
         introns.map!{ |i| i.downcase }
@@ -23,4 +18,10 @@ class ToGene
             "\tline #{lines[ind]}: #{desc}\n"
         end.join("")
     end
+
+    def self.translate_exons(exons)
+        joined_exons = exons.join("")
+        AminoAcid.translate(joined_exons)
+    end
+
 end
