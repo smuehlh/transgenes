381de83c34ceb11aa8f746e277e16a34c3929bd7
Stefanie Mühlhausen <s.muehlhausen@bath.ac.uk>
2016-03-15 12:27:33 +0000

    code now ruby -w safe

--- lib/string_extensions.rb
+++ lib/string_extensions.rb
@@ -1,11 +1,11 @@
 class String
 
     def is_upper?
-        self.match /[[:upper:]]/
+        self.match(/[[:upper:]]/)
     end
 
     def is_lower?
-        self.match /[[:lower:]]/
+        self.match(/[[:lower:]]/)
     end
 
     def regexp_delete(regexp)
