12a34be1c1504297b657b8c75ed6302ddeb73397
Stefanie Mühlhausen <s.muehlhausen@bath.ac.uk>
2016-04-08 09:57:37 +0100

    Initial commit "static pages"

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  # get 'static_pages/about', as: :
+  get '/about' => 'static_pages#about'
+
+  get '/help' => 'static_pages#help'
+
+  get '/contact' => 'static_pages#contact'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
