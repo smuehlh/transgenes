591996d71c606debd17df3760aa81fbce1ede91c
Stefanie Mühlhausen <s.muehlhausen@bath.ac.uk>
2016-04-08 10:14:29 +0100

    Set root to static_pages/about

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  # get 'static_pages/about', as: :
+  root 'static_pages#about'
+
   get '/about' => 'static_pages#about'
-
   get '/help' => 'static_pages#help'
-
   get '/contact' => 'static_pages#contact'
 
   # The priority is based upon order of creation: first created -> highest priority.
