d31c64f688aeb42ecd6ef87b8e0c1c3a0602631e
Stefanie Mühlhausen <s.muehlhausen@bath.ac.uk>
2016-04-16 17:07:52 +0100

    Changed intendation

--- app/controllers/enhancers_controller.rb
+++ app/controllers/enhancers_controller.rb
@@ -1,21 +1,21 @@
 class EnhancersController < ApplicationController
 
-  def index
-    Enhancer.delete_all
-    init_gene_enhancers
-    @enhancers = Enhancer.all
-    @enhancer = Enhancer.new
-  end
+    def index
+        Enhancer.delete_all
+        init_gene_enhancers
+        @enhancers = Enhancer.all
+        @enhancer = Enhancer.new
+    end
 
-  def create
-    @enhancers = Enhancer.all
-    # only single object with given name is allowed.
-    enhancer = Enhancer.where(name: enhancer_params[:name]).first
-    enhancer.destroy if enhancer
-    @enhancer = Enhancer.create(enhancer_params)
-  end
+    def create
+        @enhancers = Enhancer.all
+        # only single object with given name is allowed.
+        enhancer = Enhancer.where(name: enhancer_params[:name]).first
+        enhancer.destroy if enhancer
+        @enhancer = Enhancer.create(enhancer_params)
+    end
 
-  private
+    private
     def init_gene_enhancers
         Enhancer.create(name: "5'UTR")
         Enhancer.create(name: "CDS")
@@ -24,6 +24,6 @@ class EnhancersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def enhancer_params
-      params.require(:enhancer).permit(:data, :name)
+        params.require(:enhancer).permit(:data, :name)
     end
 end
