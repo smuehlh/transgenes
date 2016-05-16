69526eeb574709345f5c4bef993040b27dd89d37
Stefanie Mühlhausen <s.muehlhausen@bath.ac.uk>
2016-05-16 09:11:38 +0100

    Code refactoring.

--- app/controllers/enhancers_controller.rb
+++ app/controllers/enhancers_controller.rb
@@ -2,39 +2,32 @@ class EnhancersController < ApplicationController
     include ConvertInputToGene
 
     def index
-        delete_old_gene_enhancers
-        init_gene_enhancers
+        delete_old_init_new_gene_enhancers
         @five_enhancer, @cds_enhancer, @three_enhancer = get_gene_enhancers
         @enhancer = Enhancer.new
     end
 
     def create
         @enhancer = Enhancer.where(name: enhancer_params[:name]).first
-        # All records associated with previous input (if any) are now invalid.
-        @enhancer.records.delete_all
-
+        reset_enhancer_data_and_records
         if params[:commit] == "Reset"
-            data = ""
+            # nothing to do. enhancer was just resetted.
         else
-            gene_parser = ConvertInputToGene::ParseGene.new(enhancer_params)
-            gene_parser.get_records.each do |line, sequence|
-                record = Record.new(data: sequence, line: line)
-                @enhancer.records.push(record)
-            end
-            data = @enhancer.records.any? ? @enhancer.records.first.data : ""
-            flash[:error] = gene_parser.error
+            update_enhancer_data
         end
-        @enhancer.update_attributes(data: data)
     end
 
     private
 
-    def delete_old_gene_enhancers
-        Enhancer.delete_all
-        Record.delete_all
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def enhancer_params
+        # INFO: file is a temporary parameter only. it's not part of the model!
+        params.require(:enhancer).permit(:data, :name, :file)
     end
 
-    def init_gene_enhancers
+    def delete_old_init_new_gene_enhancers
+        Enhancer.delete_all
+        Record.delete_all
         # order does matter!
         Enhancer.create(name: "5'UTR")
         Enhancer.create(name: "CDS")
@@ -49,9 +42,21 @@ class EnhancersController < ApplicationController
         ]
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def enhancer_params
-        # INFO: file is a temporary parameter only. it's not part of the model!
-        params.require(:enhancer).permit(:data, :name, :file)
+    def reset_enhancer_data_and_records
+        # records associated with previous input (if any) are invalid.
+        @enhancer.records.delete_all
+        @enhancer.update_attributes(data: "")
+    end
+
+    def update_enhancer_data
+        gene_parser = ConvertInputToGene::ParseGene.new(enhancer_params)
+        gene_parser.get_records.each do |line, sequence|
+            record = Record.new(data: sequence, line: line)
+            @enhancer.records.push(record)
+        end
+        flash[:error] = gene_parser.error
+
+        data = @enhancer.records.any? ? @enhancer.records.first.data : ""
+        @enhancer.update_attributes(data: data)
     end
 end
