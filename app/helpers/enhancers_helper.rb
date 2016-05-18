5e7fb5ed72bdd5f9f8feb1a932058eb6b9578f72
Stefanie Mühlhausen <s.muehlhausen@bath.ac.uk>
2016-05-18 11:13:27 +0100

    Added helper method to break single words and tease them with ellipsis

--- app/helpers/enhancers_helper.rb
+++ app/helpers/enhancers_helper.rb
@@ -1,16 +1,15 @@
 module EnhancersHelper
-    def breaking_word_wrap(text, *args)
-        options = args.extract_options!
-        unless args.blank?
-            options[:line_width] = args[0] || 80
+    def word_wrap_breaking_single_word(word, maxlength)
+        if word.size > maxlength
+            word.scan(/.{1,#{maxlength}}/).join("\n")
+        else
+            word
         end
-        options.reverse_merge!(:line_width => 80)
-        text = text.split(" ").collect do |word|
-            word.length > options[:line_width] ? word.gsub(/(.{1,#{options[:line_width]}})/, "\\1 ") : word
-        end * " "
-        text.split("\n").collect do |line|
-            line.length > options[:line_width] ? line.gsub(/(.{1,#{options[:line_width]}})(\s+|$)/, "\\1\n").strip : line
-        end * "\n"
+    end
+
+    def teaser_wrapped_single_word_with_ellipsis(wrapped_word)
+        teaser = wrapped_word.split("\n").first
+        wrapped_word.include?("\n") ? "#{teaser}..." : teaser
     end
 
     def html_compatible_enhancer_name(name)
