411882b200f92540b5a8f2a194b5d7da3fc54b03
Stefanie Mühlhausen <s.muehlhausen@bath.ac.uk>
2016-05-19 11:01:50 +0100

    Reset everything inside input-form

--- app/assets/javascripts/enhancers/input.js
+++ app/assets/javascripts/enhancers/input.js
@@ -7,6 +7,9 @@ function init_input_partial() {
     var forms = $("form");
     forms.find("[id^=text-alert]").hide();
     forms.find(":submit").prop('disabled', true);
+    forms.find("textarea").val('');
+    forms.find("input:file").val('');
+    forms.find("[id^=multigene-options]").empty();
 };
 
 function init_preview_partial() {
