f524379c0d865d888151066b9ab4cc9e8a71f52e
Stefanie Mühlhausen <s.muehlhausen@bath.ac.uk>
2016-05-31 13:34:28 +0100

    Reverted exons/introns columns in enhancer table

--- db/migrate/20160531104951_add_exons_introns_to_enhancer.rb
+++
@@ -1,6 +0,0 @@
-class AddExonsIntronsToEnhancer < ActiveRecord::Migration
-  def change
-    add_column :enhancers, :exons, :text, array:true, default: []
-    add_column :enhancers, :introns, :text, array:true, default: []
-  end
-end
