ece30292fec5c100b019d227625abe2949c4224b
Stefanie Mühlhausen <s.muehlhausen@bath.ac.uk>
2016-06-06 12:53:34 +0100

    Refactored gene-parsing.

--- lib/patch_standalone_for_web/core_extensions.rb
+++ lib/patch_standalone_for_web/core_extensions.rb
@@ -1,5 +1,24 @@
 module CoreExtensions
     module FileParsing
 
+        # future class method
+        def get_all_feature_starts(use_feature, file)
+            obj = ToGene.new(use_feature)
+            obj.split_file_into_features_and_return_feature_starts(file)
+        rescue EnhancerError
+            # re-raise error. handle in web-interface
+            raise
+        rescue StandardError => exp
+            # something went very wrong. most likely the input file is corrupt.
+            ErrorHandling.abort_with_error_message(
+                "invalid_file_format", @file_info
+            )
+        end
+
+        # future instance method
+        def split_file_into_features_and_return_feature_starts(file)
+            ensure_file_format_and_split_file_into_features(file)
+            @feature_records_by_feature_starts.keys
+        end
     end
 end
--- lib/patch_standalone_for_web/webinput_to_gene.rb
+++ lib/patch_standalone_for_web/webinput_to_gene.rb
@@ -1,23 +1,23 @@
 class WebinputToGene
+    ToGene.extend CoreExtensions::FileParsing
     ToGene.include CoreExtensions::FileParsing
 
     attr_reader :error
 
     def initialize(enhancer_params, is_fileupload_input)
         @error = nil
+        @starting_lines_with_gene_records = {}
 
         feature_type = enhancer_params[:name]
         file = get_fileupload_path_or_save_textinput_to_file(
             enhancer_params, is_fileupload_input)
 
+        parse_file_and_get_gene_records(feature_type, file)
         delete_textinput_file(file)
     end
 
     def get_records
-        # dummy entry!
-        {
-            1 => {sequence: "ATG", exons: ["ATG", "TAG"], introns: ["yy"]}
-        }
+        @error.nil? ? @gene_records : {}
     end
 
     private
@@ -54,5 +54,22 @@ class WebinputToGene
     def delete_textinput_file(file)
         # remove tempfile (was created only in case of textinput)
         file.delete if file.kind_of?(Tempfile)
+    end
+
+    def parse_file_and_get_gene_records(feature_type, file)
+        @gene_records = {}
+        gene = Gene.new
+        starting_lines = ToGene.get_all_feature_starts(feature_type, file)
+        starting_lines.each do |line|
+            gene.add_cds(*ToGene.init_and_parse(feature_type, file, line))
+            @gene_records[line] = {
+                exons: gene.exons,
+                introns: gene.introns,
+                sequence: gene.sequence,
+                description: gene.description
+            }
+        end
+    rescue EnhancerError => exception
+        @error = exception.to_s
     end
 end
