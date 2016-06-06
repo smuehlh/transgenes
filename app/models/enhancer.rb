8a4df817a29571d382f0fe1401815e1b621688a1
Stefanie Mühlhausen <s.muehlhausen@bath.ac.uk>
2016-06-06 11:19:46 +0100

    Use 'serialize' to store exons and introns arrays to models.

--- app/models/enhancer.rb
+++ app/models/enhancer.rb
@@ -2,11 +2,14 @@ class Enhancer < ActiveRecord::Base
     has_many :records, dependent: :destroy
     validates :name, presence: true
 
+    serialize :exons
+    serialize :introns
+
     def reset_all_sequence_data
         self.records.delete_all
         self.update_attributes(data: "")
-        self.update_attributes(exons: "")
-        self.update_attributes(introns: "")
+        self.update_attributes(exons: [])
+        self.update_attributes(introns: [])
     end
 
     def set_all_sequence_data(record)
--- app/models/record.rb
+++ app/models/record.rb
@@ -1,3 +1,6 @@
 class Record < ActiveRecord::Base
     belongs_to :enhancer
+
+    serialize :exons
+    serialize :introns
 end
