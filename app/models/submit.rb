c0b1bd685ea88612265cb36894fd835e74f2c001
Stefanie Mühlhausen <s.muehlhausen@bath.ac.uk>
2016-06-10 09:48:46 +0100

    Updated parameter selection.

--- app/models/submit.rb
+++ app/models/submit.rb
@@ -1,5 +1,6 @@
 class Submit
     include ActiveModel::Model
 
-    attr_accessor :keep_first_intron
+    attr_accessor :keep_first_intron, :avoid_CpG, :adjust_nt_usage_along_cds,
+        :strategy
 end
