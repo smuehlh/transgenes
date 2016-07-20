21c82e678f808ca9ae1bda3746918f09bbdbcd15
Stefanie Mühlhausen <s.muehlhausen@bath.ac.uk>
2016-07-20 15:24:04 +0100

    Re-factored logging setup.

--- lib/standalone/lib/logging.rb
+++ lib/standalone/lib/logging.rb
@@ -5,14 +5,14 @@ module Logging
     extend self
 
     def default_setup_commandline_tool
-        file_logger = setup("optimiser.log", Logger::INFO)
-        stderr_logger = setup(STDERR, Logger::WARN)
+        file_logger = build_logger("optimiser.log", Logger::INFO)
+        stderr_logger = build_logger(STDERR, Logger::WARN)
         [file_logger, stderr_logger]
     end
 
-    def setup(output, level)
+    def build_logger(output, level, is_simplify=false)
         logger = initialize_logger(output, level)
-        customize_output_format_of_multiline_messages(logger)
+        customize_output_format(logger, is_simplify)
         setup_graceful_exit(logger)
 
         logger
@@ -24,7 +24,7 @@ module Logging
         progname = "SequenceOptimizer"
         timeformat = "%Y-%m-%d %H:%M:%S"
         this_logger =
-            if output.instance_of?(IO)
+            if output.instance_of?(IO) || output.instance_of?(StringIO)
                 Logger.new(output)
             else
                 file = File.open(output, File::WRONLY | File::TRUNC | File::CREAT)
@@ -38,20 +38,30 @@ module Logging
         this_logger
     end
 
-    def customize_output_format_of_multiline_messages(this_logger)
-        # original_formatter = Logger::Formatter.new
+    def customize_output_format(this_logger, is_simplify)
         this_logger.formatter = proc do |severity, datetime, progname, msg|
-            msg = msg.to_s
-            patched_msg =
-                if msg.include?("\n")
-                    spacer = "\n\t"
-                    "#{spacer}#{msg.gsub("\n",spacer)}"
-                else
-                    msg
-                end
-            # original_formatter.call(severity, datetime, progname, patched_msg)
-            "#{severity} -- #{progname}: #{patched_msg}\n"
+            if is_simplify
+                simplify(severity, datetime, progname, msg)
+            else
+                prettify_multiline_messages(severity, datetime, progname, msg)
+            end
         end
+    end
+
+    def prettify_multiline_messages(severity, datetime, progname, msg)
+        msg = msg.to_s
+        patched_msg =
+            if msg.include?("\n")
+                spacer = "\n\t"
+                "#{spacer}#{msg.gsub("\n",spacer)}"
+            else
+                msg
+            end
+        "#{severity} -- #{progname}: #{patched_msg}\n"
+    end
+
+    def simplify(severity, datetime, progname, msg)
+        "#{severity} -- #{msg}\n"
     end
 
     def setup_graceful_exit(this_logger)
