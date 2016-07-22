f4fc46fa646fe0277bc23fc130548b1a9531f524
Stefanie Mühlhausen <s.muehlhausen@bath.ac.uk>
2016-07-22 16:00:44 +0100

    Code re-factoring: format-method works on gene-obj directly.

--- lib/standalone/lib/gene_to_fasta.rb
+++ lib/standalone/lib/gene_to_fasta.rb
@@ -3,13 +3,13 @@ module GeneToFasta
     extend self
 
     def write(file, gene_obj)
-        data = formatting(gene_obj.description, gene_obj.sequence)
+        data = formatting(gene_obj)
         FileHelper.write_to_file(file, data)
     end
 
-    def formatting(description, sequence)
-        str = ">#{description}\n"
-        str += split_sequence_to_fasta_lines(sequence)
+    def formatting(gene_obj)
+        str = ">#{gene_obj.description}\n"
+        str += split_sequence_to_fasta_lines(gene_obj.sequence)
     end
 
     def split_sequence_to_fasta_lines(sequence)
