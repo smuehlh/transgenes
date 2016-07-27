bedf1566e174d809482b4ec15dba44b012af6285
Stefanie Mühlhausen <s.muehlhausen@bath.ac.uk>
2016-07-27 12:12:19 +0100

    Code re-factoring. Do not use an @error-var but Logger instead.

--- lib/patch_standalone_for_web/webinput_to_options.rb
+++ lib/patch_standalone_for_web/webinput_to_options.rb
@@ -1,20 +1,20 @@
 class WebinputToOptions
 
-    attr_reader :strategy, :remove_first_intron, :error
+    attr_reader :strategy, :remove_first_intron
 
     def initialize(submit_params)
-        CoreExtensions::Settings.setup("logger")
-
-        @error = ""
-
+        CoreExtensions::Settings.setup("logger") unless $logger
         @remove_first_intron = ! submit_params[:keep_first_intron]
+
         @strategy =
             case submit_params[:strategy]
             when "humanize" then "humanize"
             when "gc" then "gc"
             when "raw" then "raw"
             else
-                @error = "Unknown strategy #{submit_params[:strategy]}"
+                # this should never happen. 'raw' seems to be a good fall-back, though.
+                $logger.warn "Passed an invalid strategy. Will use strategy 'raw' instead."
+                "raw"
             end
     end
 end
