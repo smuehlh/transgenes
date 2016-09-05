16e5b1df15b202cd871ec70a4b80064d6f575af0
Stefanie Mühlhausen <s.muehlhausen@bath.ac.uk>
2016-09-05 13:21:23 +0100

    Finalized rake task: autocompletion:setup

--- app/models/ensembl_gene.rb
+++ app/models/ensembl_gene.rb
@@ -1,2 +1,9 @@
 class EnsemblGene < ActiveRecord::Base
+
+    def self.import(ids, seqs, version)
+        ids.each_with_index do |id, ind|
+            seq = seqs[ind]
+            create(sequence: seq, gene_id: id, version: version)
+        end
+    end
 end
