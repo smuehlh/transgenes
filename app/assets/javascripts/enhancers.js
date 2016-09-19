db3769b7cd160d56ae2db678367825b63db54660
Stefanie Mühlhausen <s.muehlhausen@bath.ac.uk>
2016-09-19 10:03:16 +0100

    Using dedicated class for form validation styling

--- app/assets/javascripts/enhancers.js
+++ app/assets/javascripts/enhancers.js
@@ -10,10 +10,10 @@ $.validator.setDefaults({
     errorElement: "span",
     errorClass: "help-block",
     highlight: function (element, errorClass, validClass) {
-        $(element).closest('.form-group').addClass('has-error');
+        $(element).closest('.form-group-validation').addClass('has-error');
     },
     unhighlight: function (element, errorClass, validClass) {
-        $(element).closest('.form-group').removeClass('has-error');
+        $(element).closest('.form-group-validation').removeClass('has-error');
     },
     errorPlacement: function (error, element) {
         if (element.parent('.input-group').length || element.prop('type') === 'checkbox' || element.prop('type') === 'radio') {
