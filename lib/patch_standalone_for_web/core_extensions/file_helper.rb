b95c80314a8f2747156d1cb977717ce47946936a
Stefanie Mühlhausen <s.muehlhausen@bath.ac.uk>
2016-09-26 09:24:38 +0100

    Bugfix: keep tempfile textinput is written to open until it is parsed.

--- lib/patch_standalone_for_web/core_extensions/file_helper.rb
+++ lib/patch_standalone_for_web/core_extensions/file_helper.rb
@@ -4,13 +4,13 @@ module CoreExtensions
         def write_to_temp_file(basename, extension, data)
             file = Tempfile.new([basename, extension])
             file.write(data)
-            file.close
+            file.flush
             file
         end
 
         def delete_temp_file(file)
             # remove tempfile (only if it is an tempfile)
-            file.delete if file.kind_of?(Tempfile)
+            file.close! if file.kind_of?(Tempfile)
         end
     end
 end
