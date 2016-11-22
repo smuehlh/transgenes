978c7fc353e4cf1248237204cf8e8f5c475a3b9f
Stefanie Mühlhausen <s.muehlhausen@bath.ac.uk>
2016-11-22 09:21:04 +0000

    Select gene by GC content

--- lib/standalone/lib/gene_enhancer.rb
+++ lib/standalone/lib/gene_enhancer.rb
@@ -14,10 +14,24 @@ class GeneEnhancer
     end
 
     def select_best_gene
-        @enhanced_genes.first
+        gc_contents = @enhanced_genes.collect{|gene| gene.gc_content}
+        distances_to_target = gc_contents.collect{|gc| (gc - target_gc_content).abs}
+        ind_best_gene = distances_to_target.index(distances_to_target.min)
+
+        @enhanced_genes[ind_best_gene]
     end
 
     private
+
+    def target_gc_content
+        if @enhanced_genes.first.introns == 0
+            # aim for the overall gc-content of 1-exon genes
+            0.526380034851897
+        else
+            # aim for the gc-content of 2-exon genes
+            0.5464775145490496
+        end
+    end
 
     def generate_synonymous_variant(gene)
         copy = Marshal.load(Marshal.dump(gene))
