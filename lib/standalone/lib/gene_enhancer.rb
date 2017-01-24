4d2af46f86c7f7fc488d03740ae645390e8e8e1b
Stefanie Mühlhausen <s.muehlhausen@bath.ac.uk>
2017-01-24 12:01:15 +0000

    Calc GC3 only once when selecting the best variant
    Making the variant generation and selection a tiny bit faster by
    calculating GC3 content only once per gene.

--- lib/standalone/lib/gene_enhancer.rb
+++ lib/standalone/lib/gene_enhancer.rb
@@ -5,18 +5,21 @@ class GeneEnhancer
         @stay_in_subbox_for_6folds = stay_in_subbox_for_6folds
 
         @enhanced_genes = []
+        @gc3_contents = []
     end
 
     def generate_synonymous_genes(gene)
-        @enhanced_genes = 1000.times.collect do |num|
+        1000.times do |num|
             @variant_number = Counting.ruby_to_human(num)
-            generate_synonymous_variant(gene)
+            gene, gc3 = generate_synonymous_variant(gene)
+
+            @enhanced_genes.push gene
+            @gc3_contents.push gc3
         end
     end
 
     def select_best_gene
-        gc3_contents = @enhanced_genes.collect{|gene| gene.gc3_content}
-        distances_to_target = gc3_contents.collect{|gc| (gc - target_gc_content).abs}
+        distances_to_target = @gc3_contents.collect{|gc| (gc - target_gc_content).abs}
         ind_best_gene = distances_to_target.index(distances_to_target.min)
         log_selection(ind_best_gene)
 
@@ -38,15 +41,16 @@ class GeneEnhancer
     def generate_synonymous_variant(gene)
         copy = Marshal.load(Marshal.dump(gene))
         copy.tweak_sequence(@strategy, @stay_in_subbox_for_6folds)
-        log_generated_variant(copy)
+        gc3 = gene.gc3_content
+        log_generated_variant(copy, gc3)
 
-        copy
+        [copy, gc3]
     end
 
-    def log_generated_variant(gene)
-        gc = to_pct(gene.gc3_content)
+    def log_generated_variant(gene, gc3)
+        gc3 = to_pct(gc3)
         n_mutated_sites, mutated_sites = gene.log_changed_sites
-        desc = "Variant #{@variant_number}: #{gc}% GC3, #{n_mutated_sites} changed sites"
+        desc = "Variant #{@variant_number}: #{gc3}% GC3, #{n_mutated_sites} changed sites"
 
         $logger.info GeneToFasta.new(desc, gene.sequence).fasta
         $logger.debug mutated_sites
@@ -55,7 +59,7 @@ class GeneEnhancer
     def log_selection(variant_ind)
         variant_number = Counting.ruby_to_human(variant_ind)
         target = to_pct(target_gc_content)
-        selected = to_pct(@enhanced_genes[variant_ind].gc3_content)
+        selected = to_pct(@gc3_contents[variant_ind])
         $logger.info "Target GC3 content: #{target}%"
         $logger.info "Closest match: Variant #{variant_number} (#{selected})%"
     end
