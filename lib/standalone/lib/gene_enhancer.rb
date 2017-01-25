0b44e3acf5665b96e847fc520ae559f27dcb2c94
Stefanie Mühlhausen <s.muehlhausen@bath.ac.uk>
2017-01-25 13:18:47 +0000

    Apply GC3 instead of GC as target

--- lib/standalone/lib/gene_enhancer.rb
+++ lib/standalone/lib/gene_enhancer.rb
@@ -19,7 +19,7 @@ class GeneEnhancer
     end
 
     def select_best_gene
-        distances_to_target = @gc3_contents.collect{|gc| (gc - target_gc_content).abs}
+        distances_to_target = @gc3_contents.collect{|gc| (gc - target_gc3).abs}
         ind_best_gene = distances_to_target.index(distances_to_target.min)
         log_selection(ind_best_gene)
 
@@ -28,13 +28,13 @@ class GeneEnhancer
 
     private
 
-    def target_gc_content
+    def target_gc3
         if @enhanced_genes.first.introns == 0
-            # aim for the overall gc-content of 1-exon genes
-            0.526380034851897
+            # aim for the average gc3-content of 1-exon genes
+            0.6058148688792696
         else
-            # aim for the gc-content of 2-exon genes
-            0.5464775145490496
+            # aim for the average gc3-content of 2-exon genes
+            0.6175028566428371
         end
     end
 
@@ -58,7 +58,7 @@ class GeneEnhancer
 
     def log_selection(variant_ind)
         variant_number = Counting.ruby_to_human(variant_ind)
-        target = to_pct(target_gc_content)
+        target = to_pct(target_gc3)
         selected = to_pct(@gc3_contents[variant_ind])
         $logger.info "Target GC3 content: #{target}%"
         $logger.info "Closest match: Variant #{variant_number} (#{selected})%"
