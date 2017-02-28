f7ddf63971ea1571682a082630375604e88a8913
Stefanie Mühlhausen <s.muehlhausen@bath.ac.uk>
2017-02-28 08:30:46 +0000

    Plot GC3 content as line- rather than bubble-chart

--- app/assets/javascripts/enhancers/chartSettings.js
+++ app/assets/javascripts/enhancers/chartSettings.js
@@ -1,18 +1,27 @@
 Chart.defaults.global.legend.display = false;
 Chart.defaults.global.animation = false;
 Chart.defaults.global.elements.point.backgroundColor = "rgb(51, 122, 183)"; // Bootstrap primary color
+Chart.defaults.line.showLines = false;
 var chartOptions = {
     scales: {
         xAxes: [{
             gridLines: {
                 display: false,
                 color: "rgba(0, 0, 0, 0.5)"
+            },
+            scaleLabel: {
+                display: true,
+                labelString: 'Position in Gene (codons)'
             }
         }],
         yAxes: [{
             gridLines: {
                 display: false,
                 color: "rgba(0, 0, 0, 0.5)"
+            },
+            scaleLabel: {
+                display: true,
+                labelString: '% GC3'
             }
         }]
     }
