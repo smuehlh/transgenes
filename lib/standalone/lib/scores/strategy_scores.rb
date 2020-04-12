1e535a899c6357facce9a51965700a77bd9415b4
Stefanie Mühlhausen <s.muehlhausen@bath.ac.uk>
2020-04-12 19:43:20 +0200

    Implement 'attenuate' strategy

--- lib/standalone/lib/scores/strategy_scores.rb
+++ lib/standalone/lib/scores/strategy_scores.rb
@@ -52,6 +52,8 @@ class StrategyScores
             gc_count(synonymous_codon, pos, is_near_intron, dist_to_intron)
         when "max-gc"
             max_gc_count(synonymous_codon)
+        when "attenuate"
+            attenuate_count(synonymous_codon, next_codon, pos, is_near_intron, dist_to_intron)
         end
     end
 
@@ -77,5 +79,38 @@ class StrategyScores
 
     def max_gc_count(synonymous_codon)
         Maximal_gc3[synonymous_codon]
+    end
+
+    def attenuate_count(synonymous_codon, next_codon, pos, is_near_intron, dist_to_intron)
+        # ensure there is a next codons before relying on it
+        # (although in practice this should only be a problem if the sequence
+        # does not end with a stop codon)
+        if _generates_CpG?(synonymous_codon, next_codon)
+            addend = synonymous_codon.count("AT")/3.to_f
+            1 + addend
+        elsif _generates_TpA?(synonymous_codon, next_codon)
+            addend = synonymous_codon.count("AT")/3.to_f
+            # tweak addend to ensure CpGs still rank higher
+            0.99 + addend*(1-0.99)
+        else
+            # score by usage in human genes while avoiding C's and G's
+            multiplier = 1 - synonymous_codon.count("GC")/3.to_f
+            (1 - gc_count(
+                synonymous_codon, pos, is_near_intron, dist_to_intron)
+            )*multiplier
+        end
+    end
+
+    def _generates_CpG?(synonymous_codon, next_codon)
+        ( next_codon && synonymous_codon.end_with?("C") && next_codon.start_with?("G")
+        ) || synonymous_codon.include?("CG")
+        # 'include' rather than 'end_with' to catch CpGs both
+        #   at first/second and at second/third position
+        # (not neccessary for TpAs due to structure of codon boxes)
+    end
+
+    def _generates_TpA?(synonymous_codon, next_codon)
+        ( next_codon && synonymous_codon.end_with?("T") && next_codon.start_with?("A")
+        ) || synonymous_codon.end_with?("TA")
     end
 end
