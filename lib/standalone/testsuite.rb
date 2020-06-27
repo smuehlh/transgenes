90c38c8baf5d4e8eb5ebe1f59dc7d0067a9c51f1
Stefanie Mühlhausen <s.muehlhausen@bath.ac.uk>
2020-06-27 22:12:26 +0200

    Adjust test suite to test for 'good' variants

--- lib/standalone/testsuite.rb
+++ lib/standalone/testsuite.rb
@@ -4,8 +4,8 @@ require 'ostruct'
 
 =begin
     A QuickTest style test suite:
-    generate short, random sequences and check if key characteristics of an
-    attenuated sequence are met.
+    Generate random sequences and check if generated variants meet
+    key characteristics of attenuated sequences.
 
 =end
 
@@ -14,31 +14,14 @@ Dir[File.join(File.dirname(__FILE__), 'lib', '**', '*.rb')].each do |file|
     require File.absolute_path(file)
 end
 
-def characterise(sequence)
-    OpenStruct.new(
-        dnaseq: sequence,
-        protseq: GeneticCode.translate(sequence),
-        GC: sequence.count("GC"),
-        T: sequence.count("T"),
-        CpG: sequence.scan("CG").length,
-        UpA: sequence.scan("TA").length,
-    )
-end
-
-def characterise_variants(seqs)
-    OpenStruct.new(
-        dnaseq: seqs.first,
-        protseq: GeneticCode.translate(seqs.first),
-        GC: Statistics.mean(seqs.collect{|s| s.count("GC")}),
-        T: Statistics.mean(seqs.collect{|s| s.count("T")}),
-        CpG: Statistics.mean(seqs.collect{|s| s.scan("CG").size}),
-        UpA: Statistics.mean(seqs.collect{|s| s.scan("TA").size}),
-    )
-end
-
-def tweak_gene_by(gene, strategy, cpg_enrichment)
+def tweak_gene(gene)
+    cpg_enrichment = rand(0..1.5)
+    tpa_enrichment = rand(0.6..1.4)
     options = OpenStruct.new(
-        strategy: strategy, CpG_enrichment: cpg_enrichment, stay_in_subbox_for_6folds: false
+        strategy: "attenuate",
+        CpG_enrichment: cpg_enrichment,
+        TpA_enrichment: tpa_enrichment,
+        stay_in_subbox_for_6folds: false
     )
     enhancer = GeneEnhancer.new(options)
     enhancer.generate_synonymous_genes(gene)
@@ -48,126 +31,83 @@ def tweak_gene_by(gene, strategy, cpg_enrichment)
         lines = fasta.split("\n")
         variants.push lines[1..-1].join("")
     end
+    [variants, cpg_enrichment, tpa_enrichment]
+end
+
+def filter_variants_by_GC(variants, gene, cpg_enrichment)
     # calculate target G+C content
     original_GC = gene.sequence.count("GC") / gene.sequence.size.to_f
-    max_GC = original_GC + original_GC * (1 - cpg_enrichment)
+    max_GC =
+        if cpg_enrichment < 1
+            original_GC + original_GC * (1 - cpg_enrichment)
+        else
+            original_GC
+        end
 
     # select variants below maximum tolerable GC
-    variants.select do
-        |variant| variant.count("GC") / gene.sequence.size.to_f <= max_GC
+    variants.select do |variant|
+        variant.count("GC") / gene.sequence.size.to_f <= max_GC
     end
 end
 
-def check_key_characteristics_for_attenuate_strategy_low_CpGe(before, after)
-    flags = []
-    unless before.dnaseq != after.dnaseq
-        flags.push "seq unchanged"
-    end
-    unless before.protseq == after.protseq
-        flags.push "seq failed"
-    end
-    unless after.CpG >= before.CpG
-        flags.push "CpG failed"
-    end
-    unless after.T <= before.T
-        flags.push "T failed"
-    end
-    unless after.GC <= before.GC * 2
-        flags.push "selection by GC failed"
-    end
-    flags
-end
-
-def check_key_characteristics_for_attenuate_strategy_mean_CpGe(before, after)
-    flags = []
-    unless before.dnaseq != after.dnaseq
-        flags.push "seq unchanged"
-    end
-    unless before.protseq == after.protseq
-        flags.push "seq failed"
-    end
-    # unless after.CpG = before.CpG
-    #     flags.push "CpG failed"
-    # end
-    # unless after.T <= before.T
-    #     flags.push "T failed"
-    # end
-    unless after.GC <= before.GC + before.GC * 0.5
-        flags.push "selection by GC failed"
-    end
-    flags
-end
-
-def check_key_characteristics_for_attenuate_strategy_high_CpGe(before, after)
-    flags = []
-    unless before.dnaseq != after.dnaseq
-        flags.push "seq unchanged"
-    end
-    unless before.protseq == after.protseq
-        flags.push "seq failed"
-    end
-    unless after.CpG <= before.CpG
-        flags.push "CpG failed"
-    end
-    unless after.T >= before.T
-        flags.push "T failed"
-    end
-    unless after.GC <= before.GC
-        flags.push "selection by GC failed"
-    end
-    flags
+def characterise_seq(seq)
+    OpenStruct.new(
+        transl: GeneticCode.translate(seq),
+        U: seq.count("T"),
+        CpG: seq.scan("CG").size,
+        UpA: seq.scan("TA").size,
+    )
 end
 
 Logging.setup
 
-100.times do |n|
-    # randomly build sequence
-    codons = GeneticCode.valid_codons.reject{|c| GeneticCode.is_stopcodon(c)}
-    seq = "ATG"
-    5.times do |sample|
-        seq += codons.sample
+# randomly build sequence
+codons = GeneticCode.valid_codons.reject{|c| GeneticCode.is_stopcodon(c)}
+seq = "ATG"
+100.times do |sample|
+    seq += codons.sample
+end
+seq += GeneticCode.valid_codons.reject{|c| !GeneticCode.is_stopcodon(c)}.sample
+
+# generate sequence variants
+gene = Gene.new
+gene.add_cds([seq], [], "test")
+variants, cpg_enrichment, tpa_enrichment = tweak_gene(gene)
+variants = filter_variants_by_GC(variants, gene, cpg_enrichment)
+
+# test variants
+if variants.empty?
+    puts "variant selection failed, none remain"
+end
+
+before = characterise_seq(gene.sequence)
+n_good_variants = 0
+variants.each_with_index do |v_seq, ind|
+    after = characterise_seq(v_seq)
+    if seq == v_seq
+        puts "variant [#{ind}]: sequence unchanged"
     end
-    seq += "TAA"
-
-    gene = Gene.new
-    gene.add_cds([seq], [], "test")
-    before = characterise(gene.sequence)
-
-    # strategy attenuate
-    enhanced_gene_seqs = tweak_gene_by(gene, "attenuate", 0)
-    after = characterise_variants(enhanced_gene_seqs)
-
-    flags = check_key_characteristics_for_attenuate_strategy_low_CpGe(before, after)
-    if flags.any?
-        puts "::attenuate/ CpGe = 0:: ##{n} failed [#{flags.join(", ")}]: "
-            puts "\t#{GeneticCode.split_cdna_into_codons(gene.sequence).join(" ")} => #{GeneticCode.split_cdna_into_codons(enhanced_gene_seqs.first).join(" ")}\n"
+    if before.protseq != after.protseq
+        puts "variant [#{ind}]: made non-synonymous changes"
     end
-
-    enhanced_gene_seqs = tweak_gene_by(gene, "attenuate", 0.5)
-    after = characterise_variants(enhanced_gene_seqs)
-    flags = check_key_characteristics_for_attenuate_strategy_mean_CpGe(before, after)
-    if flags.any?
-        puts "::attenuate/ CpGe = 0.5:: ##{n} failed [#{flags.join(", ")}]: "
-            puts "\t#{GeneticCode.split_cdna_into_codons(gene.sequence).join(" ")} => #{GeneticCode.split_cdna_into_codons(enhanced_gene_seqs.first).join(" ")}\n"
-    end
-
-    enhanced_gene_seqs = tweak_gene_by(gene, "attenuate", 1)
-    after = characterise_variants(enhanced_gene_seqs)
-    flags = check_key_characteristics_for_attenuate_strategy_high_CpGe(before, after)
-    if flags.any?
-        puts "::attenuate/ CpGe = 1:: ##{n} failed [#{flags.join(", ")}]: "
-            puts "\t#{GeneticCode.split_cdna_into_codons(gene.sequence).join(" ")} => #{GeneticCode.split_cdna_into_codons(enhanced_gene_seqs.first).join(" ")}\n"
+    if v_seq[-3..-1] != "TAA"
+        puts "variant [#{ind}]: didn't set stop to TAA"
     end
 
-    enhanced_gene_seqs = tweak_gene_by(gene, "attenuate", 1.4)
-    if enhanced_gene_seqs.size == 0
-        puts "::attenuate/ CpGe > 1:: ##{n} failed: none remaining"
-        next
+    if cpg_enrichment <= 1
+        if after.CpG > before.CpG && after.U > before.U
+            n_good_variants += 1
+        end
+    else
+        if after.U > before.U
+            n_good_variants += 1
+        end
     end
-    after = characterise_variants(enhanced_gene_seqs)
-    flags = check_key_characteristics_for_attenuate_strategy_high_CpGe(before, after)
-    if flags.any?
-        puts "::attenuate/ CpGe > 1:: ##{n} failed [#{flags.join(", ")}]: "
-            puts "\t#{GeneticCode.split_cdna_into_codons(gene.sequence).join(" ")} => #{GeneticCode.split_cdna_into_codons(enhanced_gene_seqs.first).join(" ")}\n"
-    end
+end
+
+if n_good_variants == 0
+    puts "variant generation failed, none has wanted characteristics"
+else
+    puts "#{n_good_variants}/#{variants.size} variants qualify"
+    puts "target CpG: #{cpg_enrichment}, target UpA: #{tpa_enrichment}"
 end
